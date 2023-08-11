90d1c329bef2b863e75dd7a0a46e4f117fb00482
aperushin <aperushin@gmail.com>
2023-08-11 12:04:02 +0300

    Remove unused 'role' field from User model

--- todolist/core/models.py
+++ todolist/core/models.py
@@ -1,16 +1,8 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 
 
-class UserRoles(models.TextChoices):
-    USER = ("user", _("User"))
-    ADMIN = ("admin", _("Admin"))
-
-
 class User(AbstractUser):
-    role = models.CharField(max_length=5, choices=UserRoles.choices, default=UserRoles.USER)
-
     def __str__(self):
         return self.username
 
--- todolist/tests/factories.py
+++ todolist/tests/factories.py
@@ -2,7 +2,7 @@ import factory
 from django.contrib.auth.hashers import make_password
 from django.utils import timezone
 
-from core.models import User, UserRoles
+from core.models import User
 from goals.models import GoalCategory
 
 USER_PASSWORD = '123afafa'
@@ -17,7 +17,6 @@ class UserFactory(factory.django.DjangoModelFactory):
     email = factory.Faker('email')
     first_name = factory.Faker('name')
     last_name = factory.Faker('name')
-    role = UserRoles.USER
     is_active = True
 
 
