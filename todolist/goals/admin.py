722ab6890dff729fa4ef84db3d2989f446050c59
aperushin <aperushin@gmail.com>
2023-08-15 13:11:59 +0300

    Add Board and BoardParticipant models to admin panel

--- todolist/goals/admin.py
+++ todolist/goals/admin.py
@@ -1,23 +1,40 @@
 from django.contrib import admin
 
-from goals.models import GoalCategory, Goal, GoalComment
+from goals.models import GoalCategory, Goal, GoalComment, Board, BoardParticipant
 
 
 class GoalCategoryAdmin(admin.ModelAdmin):
     list_display = ('title', 'user', 'created', 'updated')
-    search_fields = ('title', 'user')
+    search_fields = ('title', 'user__username')
+    readonly_fields = ('created', 'updated')
 
 
 class GoalAdmin(admin.ModelAdmin):
     list_display = ('title', 'user', 'created', 'updated')
-    search_fields = ('title', 'user', 'description')
+    search_fields = ('title', 'user__username', 'description')
+    readonly_fields = ('created', 'updated')
 
 
 class GoalCommentAdmin(admin.ModelAdmin):
     list_display = ('text', 'user', 'created', 'updated')
-    search_fields = ('text', 'user')
+    search_fields = ('text', 'user__username')
+    readonly_fields = ('created', 'updated')
+
+
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created', 'updated')
+    search_fields = ('title', )
+    readonly_fields = ('created', 'updated')
+
+
+class BoardParticipantAdmin(admin.ModelAdmin):
+    list_display = ('user', 'board', 'role', 'created', 'updated')
+    search_fields = ('user__username', )
+    readonly_fields = ('created', 'updated')
 
 
 admin.site.register(GoalCategory, GoalCategoryAdmin)
 admin.site.register(Goal, GoalAdmin)
 admin.site.register(GoalComment, GoalCommentAdmin)
+admin.site.register(Board, BoardAdmin)
+admin.site.register(BoardParticipant, BoardParticipantAdmin)
