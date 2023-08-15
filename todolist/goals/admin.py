620ae6fd60de0e584ec6828e776c5a973ab3c024
aperushin <aperushin@gmail.com>
2023-08-15 18:39:54 +0300

    Refactor model registration in admin panel

--- todolist/goals/admin.py
+++ todolist/goals/admin.py
@@ -3,38 +3,36 @@ from django.contrib import admin
 from goals.models import GoalCategory, Goal, GoalComment, Board, BoardParticipant
 
 
+@admin.register(GoalCategory)
 class GoalCategoryAdmin(admin.ModelAdmin):
     list_display = ('title', 'user', 'created', 'updated')
     search_fields = ('title', 'user__username')
     readonly_fields = ('created', 'updated')
 
 
+@admin.register(Goal)
 class GoalAdmin(admin.ModelAdmin):
     list_display = ('title', 'user', 'created', 'updated')
     search_fields = ('title', 'user__username', 'description')
     readonly_fields = ('created', 'updated')
 
 
+@admin.register(GoalComment)
 class GoalCommentAdmin(admin.ModelAdmin):
     list_display = ('text', 'user', 'created', 'updated')
     search_fields = ('text', 'user__username')
     readonly_fields = ('created', 'updated')
 
 
+@admin.register(Board)
 class BoardAdmin(admin.ModelAdmin):
     list_display = ('title', 'created', 'updated')
     search_fields = ('title', )
     readonly_fields = ('created', 'updated')
 
 
+@admin.register(BoardParticipant)
 class BoardParticipantAdmin(admin.ModelAdmin):
     list_display = ('user', 'board', 'role', 'created', 'updated')
     search_fields = ('user__username', )
     readonly_fields = ('created', 'updated')
-
-
-admin.site.register(GoalCategory, GoalCategoryAdmin)
-admin.site.register(Goal, GoalAdmin)
-admin.site.register(GoalComment, GoalCommentAdmin)
-admin.site.register(Board, BoardAdmin)
-admin.site.register(BoardParticipant, BoardParticipantAdmin)
