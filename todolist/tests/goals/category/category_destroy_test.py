30295933898e3a061e974e7704481cac64717b30
aperushin <aperushin@gmail.com>
2023-08-15 20:47:13 +0300

    Fix goal category tests after Board model addition

--- todolist/tests/goals/category/category_destroy_test.py
+++ todolist/tests/goals/category/category_destroy_test.py
@@ -1,17 +1,17 @@
 import pytest
 
 from tests.factories import UserFactory, USER_PASSWORD, GoalCategoryFactory
-from goals.models import GoalCategory
+from goals.models import GoalCategory, Board, BoardParticipant
 
 
 @pytest.mark.django_db
-def test_destroy_category(client, user: UserFactory):
+def test_destroy_category(client, user: UserFactory, board: Board, board_participant: BoardParticipant):
     """
     Test successfully getting a category
     """
     client.login(username=user.username, password=USER_PASSWORD)
 
-    category: GoalCategory = GoalCategoryFactory.create(user=user)
+    category: GoalCategory = GoalCategoryFactory.create(user=user, board=board)
 
     response = client.delete(f'/goals/goal_category/{category.id}')
 
