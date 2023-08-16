00fbe334a2f5b23b2d2cb9100c1762834be5b61d
aperushin <aperushin@gmail.com>
2023-08-16 09:10:01 +0300

    Add permissions

--- todolist/goals/permissions.py
+++ todolist/goals/permissions.py
@@ -1,7 +1,7 @@
 from rest_framework import permissions
 from rest_framework.request import Request
 
-from goals.models import BoardParticipant, Board, GoalCategory
+from goals.models import BoardParticipant, Board, GoalCategory, Goal, GoalComment
 
 
 class BoardPermission(permissions.IsAuthenticated):
@@ -20,3 +20,17 @@ class CategoryPermission(permissions.IsAuthenticated):
             _filters['role__in'] = (BoardParticipant.Role.owner, BoardParticipant.Role.writer)
 
         return BoardParticipant.objects.filter(**_filters).exists()
+
+
+class GoalPermission(permissions.IsAuthenticated):
+    def has_object_permission(self, request: Request, view, obj: Goal) -> bool:
+        _filters: dict = {'user_id': request.user.id, 'board_id': obj.category.board_id}
+        if request.method not in permissions.SAFE_METHODS:
+            _filters['role__in'] = (BoardParticipant.Role.owner, BoardParticipant.Role.writer)
+
+        return BoardParticipant.objects.filter(**_filters).exists()
+
+
+class CommentPermission(permissions.IsAuthenticated):
+    def has_object_permission(self, request: Request, view, obj: GoalComment) -> bool:
+        return request.method in permissions.SAFE_METHODS or request.user.id == obj.user_id
