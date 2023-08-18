a4f912a562658957335cb215ad9545ff586b8fef
aperushin <aperushin@gmail.com>
2023-08-18 16:01:20 +0300

    Add successful verification message for TG bot verification

--- todolist/bot/views.py
+++ todolist/bot/views.py
@@ -2,6 +2,8 @@ from rest_framework.generics import UpdateAPIView
 from rest_framework.permissions import IsAuthenticated, BasePermission
 from rest_framework.serializers import BaseSerializer
 
+from bot.models import TgUser
+from bot.tg.client import TgClient
 from core.models import User
 from bot.serializers import BotVerifySerializer
 
@@ -12,3 +14,8 @@ class BotVerifyView(UpdateAPIView):
 
     def get_object(self) -> User:
         return self.request.user
+
+    def perform_update(self, serializer: BotVerifySerializer):
+        tg_user: TgUser = serializer.save()
+        verify_success_msg = 'Congratulations! You have successfully linked your telegram account'
+        TgClient().send_message(chat_id=tg_user.tg_chat_id, text=verify_success_msg)
