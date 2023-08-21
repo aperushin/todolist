c501d18f3fb64763d0176df56d6e32269aad0e7b
aperushin <aperushin@gmail.com>
2023-08-21 17:44:44 +0300

    Add documentation, minor refactors

--- todolist/bot/management/commands/runbot.py
+++ todolist/bot/management/commands/runbot.py
@@ -15,17 +15,22 @@ logger = logging.getLogger(__name__)
 
 
 class Handler(Protocol):
-    """
-    Protocol for a callable handler
-    """
+    """Protocol for a callable handler"""
     def __call__(self, tg_user: TgUser, msg: Message = ..., cb_data: str = ...) -> None:
         ...
 
 
 class Command(BaseCommand):
-    help = 'Run telegram bot'
+    """Django management command that runs a telegram bot
 
-    # {chat_id: {'command': '/command', 'data': 'data'}}
+    Attributes:
+        help (str): Description of the command, which will be printed in help messages.
+        create_data (dict): Dictionary for storing instance creation data from users.
+            Structure: {chat_id (int): {'command': command (str), 'data': data (str)},}
+        tg_client (TgClient): An instance of the telegram client class.
+
+    """
+    help = 'Run telegram bot'
     create_data: dict = dict()
 
     def __init__(self, *args, **kwargs):
@@ -33,8 +38,11 @@ class Command(BaseCommand):
         self.tg_client: TgClient = TgClient()
 
     def handle(self, *args, **options) -> None:
-        """
-        Main loop
+        """Main loop
+
+        Gets new updates from the telegram bot, passes the data from the update to the message or callback handler
+        based on the update's contents
+
         """
         offset = 0
 
@@ -51,8 +59,15 @@ class Command(BaseCommand):
                     self.handle_callback(item.callback_query)
 
     def handle_message(self, msg: Message) -> None:
-        """
-        Handle message from a user
+        """ Handle a message from a user
+
+        Gets an existing TgUser based on the chat id from the message or creates a new one if user was not found.
+        Then sends a greeting message if the user was created, or passes data to one of two methods based on whether
+        the TgUser's 'user' field is filled (empty field means an unauthorized user).
+
+        Args:
+            msg (Message): Message object
+
         """
         tg_user: TgUser
         tg_user, created = TgUser.objects.get_or_create(chat_id=msg.chat.id)
@@ -66,47 +81,68 @@ class Command(BaseCommand):
             self.handle_authorized(tg_user, msg)
 
     def handle_callback(self, cb_query: CallbackQuery) -> None:
-        """
-        Handle callback from a user's chat
-        """
-        tg_user: TgUser = TgUser.objects.get(chat_id=cb_query.message.chat.id)
-        msg = cb_query.message
+        """Handle a callback from a user's chat
 
-        # Try parsing callback data as a command
-        command_handler = self.get_handler(cb_query.data)
+        Gets an existing TgUser based on the chat id from the callback query object, then tries to parse the callback
+        data as a command. If successful, passes the user to the respective command handler; if not, passes the
+        callback data to the handle_create method.
+
+        Args:
+            cb_query (CallbackQuery): Callback query object
+
+        """
+        msg: Message = cb_query.message
+        callback_data: str = cb_query.data
+        tg_user: TgUser = TgUser.objects.get(chat_id=msg.chat.id)
+
+        command_handler: Handler = self.get_handler(callback_data)
         if command_handler:
             command_handler(tg_user, msg=msg)
         else:
-            # If not a command, should be data for create methods
-            cb_data = cb_query.data
-            self.handle_create(tg_user, cb_data=cb_data)
+            self.handle_create(tg_user, cb_data=callback_data)
 
     def handle_unauthorized(self, tg_user: TgUser) -> None:
-        """
-        Handle an unauthorized user
+        """Handle an unauthorized user
+
+        Updates the user's verification code and sends it to the user.
+
+        Args:
+            tg_user (TgUser): Telegram user object
+
         """
         tg_user.update_verification_code()
         unauthorized_msg = f'Please enter this verification code on {settings.SITE_URL}: {tg_user.verification_code}'
         self.tg_client.send_message(chat_id=tg_user.chat_id, text=unauthorized_msg)
 
     def handle_authorized(self, tg_user: TgUser, msg: Message) -> None:
-        """
-        Handle message from an authorized user
-        """
-        command_handler = self.get_handler(msg.text)
+        """Handle message from an authorized user
 
-        # If message text is one of supported commands
+        Tries to parse message as a command, if successful, passes the data to the respective command handler.
+        If not, checks if data for instance creation methods is expected from the user and passes the data to the
+        creation handler, or sends user an error message.
+
+        Args:
+            tg_user (TgUser): Telegram user object
+            msg (Message): Message object
+
+        """
+        command_handler: Handler = self.get_handler(msg.text)
+
         if command_handler:
             command_handler(tg_user, msg)
-        # If message text is expected to contain data for create methods
         elif tg_user.chat_id in self.create_data:
             self.handle_create(tg_user, msg)
         else:
             self.tg_client.send_message(chat_id=tg_user.chat_id, text='Unknown command')
 
     def get_handler(self, command: str) -> Handler | None:
-        """
-        Return a callable method based on the given command
+        """Return a callable method based on the given command
+
+        Args:
+            command (str): Command string starting from '/'.
+
+        Returns:
+            Callable command handler method of None if a command was not found.
         """
         commands = {
             '/goals': self.handle_goals,
@@ -119,21 +155,28 @@ class Command(BaseCommand):
         return commands.get(command)
 
     def clear_creation_data(self, chat_id: int, send_error_msg: bool = False) -> None:
-        """
-        Remove the user's data from self.create_data
+        """Remove the user's data from self.create_data
 
-        :param send_error_msg: Send an error message to the user
+        Args:
+            chat_id (int): User's chat id.
+            send_error_msg (bool): If True, sends an error message to the user. Defaults to False.
+
         """
         self.create_data.pop(chat_id, None)
         if send_error_msg:
             self.tg_client.send_message(chat_id, text='Something went wrong, please start over')
 
     @staticmethod
-    def generate_buttons_markup(button_names: list[str]) -> dict:
-        """
-        Generate buttons for telegram bot markup with button names as callback data
+    def generate_buttons_markup(button_names: list[str] | QuerySet) -> dict:
+        """Generate buttons for telegram bot markup
 
-        Adds a /cancel command button to the end
+        Args:
+            button_names (str): list of button names.
+
+        Returns:
+            dict: Markup dictionary containing inline keyboard buttons with the provided button names as both
+                button text and callback data, and an additional button with '/cancel' command in callback data.
+
         """
         buttons = [[{'text': name, 'callback_data': name}] for name in button_names]
         buttons.append([{'text': '[Cancel]', 'callback_data': '/cancel'}])
@@ -142,10 +185,14 @@ class Command(BaseCommand):
     # Command handlers
 
     def handle_goals(self, tg_user: TgUser, msg: Message = None) -> None:
-        """
-        Handle /goals command
+        """Handle /goals command
 
         Sends a list of the user's goal titles to the user
+
+        Args:
+            tg_user (TgUser): Telegram user object
+            msg (:obj:`Message`, optional): Message object
+
         """
         goal_titles: QuerySet = (
             Goal.objects.filter(user_id=tg_user.user_id)
@@ -161,8 +208,16 @@ class Command(BaseCommand):
         self.tg_client.send_message(chat_id=tg_user.chat_id, text=goals_reply_msg)
 
     def handle_create(self, tg_user: TgUser, msg: Message = None, cb_data: str = None) -> None:
-        """
-        Handle /create command or data for one of the other creation commands
+        """Handle /create command or data for one of the other creation commands
+
+        Passes data to the creation handler that expects data from the user or sends user
+        a list of creation commands if data is not expected.
+
+        Args:
+            tg_user (TgUser): Telegram user object
+            msg (:obj:`Message`, optional): Message object
+            cb_data (:obj:`str`, optional): Callback data
+
         """
         chat_id: int = tg_user.chat_id
         create_data: dict = self.create_data.get(chat_id)
@@ -186,17 +241,26 @@ class Command(BaseCommand):
             self.tg_client.send_message(chat_id=chat_id, text='What would you like to create?', reply_markup=markup)
 
     def handle_cancel(self, tg_user: TgUser, msg: Message) -> None:
-        """
-        Handle /cancel command
+        """Handle /cancel command
 
-        Clear creation data for the user's chat id, send user a message
+        Removes creation data for the user's chat id from self.creation_data, sends user a message.
+
+        Args:
+            tg_user (TgUser): Telegram user object
+            msg (Message): Message object
+
         """
         self.clear_creation_data(tg_user.chat_id)
         self.tg_client.send_message(tg_user.chat_id, text='Creation cancelled')
 
     def handle_create_goal(self, tg_user: TgUser, msg: Message = None, cb_data: str = None):
-        """
-        Handle /creategoal command
+        """Handle /creategoal command
+
+        Args:
+            tg_user (TgUser): Telegram user object
+            msg (:obj:`Message`, optional): Message object
+            cb_data (:obj:`str`, optional): Callback data
+
         """
         chat_id: int = tg_user.chat_id
         create_data: dict = self.create_data.get(chat_id)
@@ -204,7 +268,7 @@ class Command(BaseCommand):
         # Step 1: creation hasn't been started yet
         if create_data is None:
             # Get existing categories' titles
-            category_titles = (
+            category_titles: QuerySet = (
                 GoalCategory.objects.filter(user_id=tg_user.user_id)
                 .exclude(is_deleted=True)
                 .values_list('title', flat=True)
@@ -229,16 +293,16 @@ class Command(BaseCommand):
 
         # Step 3: category title was saved, expecting goal title
         elif category_title := create_data.get('data'):
-            goal_title = msg.text
+            goal_title: str = msg.text
             try:
-                category = GoalCategory.objects.exclude(is_deleted=True).get(title=category_title)
+                category: GoalCategory = GoalCategory.objects.exclude(is_deleted=True).get(title=category_title)
             except GoalCategory.DoesNotExist:
                 # If category was deleted before user finished creation
                 self.clear_creation_data(chat_id, send_error_msg=True)
                 return
 
-            goal = Goal.objects.create(title=goal_title, category_id=category.id, user_id=tg_user.user.id)
-            goal_url = (
+            goal: Goal = Goal.objects.create(title=goal_title, category_id=category.id, user_id=tg_user.user.id)
+            goal_url: str = (
                 settings.SITE_URL +
                 f'/boards/{goal.category.board_id}/categories/{goal.category_id}/goals?goal={goal.id}'
             )
@@ -251,8 +315,13 @@ class Command(BaseCommand):
             self.clear_creation_data(chat_id, send_error_msg=True)
 
     def handle_create_cat(self, tg_user: TgUser, msg: Message = None, cb_data: str = None):
-        """
-        Handle /createcat command
+        """Handle /createcat command
+
+        Args:
+            tg_user (TgUser): Telegram user object
+            msg (:obj:`Message`, optional): Message object
+            cb_data (:obj:`str`, optional): Callback data
+
         """
         chat_id: int = tg_user.chat_id
         create_data: dict = self.create_data.get(chat_id)
@@ -260,7 +329,7 @@ class Command(BaseCommand):
         # Step 1: creation hasn't been started yet
         if create_data is None:
             # Get existing boards' titles
-            board_titles = (
+            board_titles: QuerySet = (
                 Board.objects.filter(
                     participants__user=tg_user.user,
                     participants__role__in=[BoardParticipant.Role.owner, BoardParticipant.Role.writer],
@@ -286,16 +355,18 @@ class Command(BaseCommand):
 
         # Step 3: board title was saved, expecting category title
         elif board_title := create_data.get('data'):
-            category_title = msg.text
+            category_title: str = msg.text
             try:
-                board = Board.objects.exclude(is_deleted=True).get(title=board_title)
+                board: Board = Board.objects.exclude(is_deleted=True).get(title=board_title)
             except Board.DoesNotExist:
                 # If board was deleted before user finished creation
                 self.clear_creation_data(chat_id, send_error_msg=True)
                 return
 
-            category = GoalCategory.objects.create(title=category_title, board_id=board.id, user_id=tg_user.user.id)
-            category_url = (
+            category: GoalCategory = GoalCategory.objects.create(
+                title=category_title, board_id=board.id, user_id=tg_user.user.id
+            )
+            category_url: str = (
                 settings.SITE_URL +
                 f'/boards/{category.board_id}/categories/{category.id}/goals'
             )
@@ -308,8 +379,13 @@ class Command(BaseCommand):
             self.clear_creation_data(chat_id, send_error_msg=True)
 
     def handle_create_board(self, tg_user: TgUser, msg: Message = None, cb_data: str = None):
-        """
-        Handle /createboard command
+        """Handle /createboard command
+
+        Args:
+            tg_user (TgUser): Telegram user object
+            msg (:obj:`Message`, optional): Message object
+            cb_data (:obj:`str`, optional): Callback data
+
         """
         chat_id: int = tg_user.chat_id
         create_data: dict = self.create_data.get(chat_id)
@@ -321,12 +397,12 @@ class Command(BaseCommand):
 
         # Step 2: creation started, expecting board title
         else:
-            board_title = msg.text
+            board_title: str = msg.text
             with transaction.atomic():
-                board = Board.objects.create(title=board_title)
+                board: Board = Board.objects.create(title=board_title)
                 BoardParticipant.objects.create(board_id=board.id, user_id=tg_user.user.id)
 
-            board_url = (
+            board_url: str = (
                 settings.SITE_URL +
                 f'/boards/{board.id}/goals'
             )
