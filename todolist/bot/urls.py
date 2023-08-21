471ce58c6521fe697458c7474efd2748af32f10b
aperushin <aperushin@gmail.com>
2023-08-21 14:02:47 +0300

    Change url names to snake-case

--- todolist/bot/urls.py
+++ todolist/bot/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from bot.views import BotVerifyView
 
 urlpatterns = [
-    path('verify', BotVerifyView.as_view(), name='verify telegram user')
+    path('verify', BotVerifyView.as_view(), name='verify-telegram-user')
 ]
--- todolist/core/urls.py
+++ todolist/core/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('signup', UserCreateView.as_view(), name='signup'),
     path('login', LoginView.as_view(), name='login'),
     path('profile', ProfileView.as_view(), name='profile'),
-    path('update_password', UpdatePasswordView.as_view(), name='update_password'),
+    path('update_password', UpdatePasswordView.as_view(), name='update-password'),
 ]
